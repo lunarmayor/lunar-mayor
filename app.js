400945b9fd363ec66863e10677143681a92a0746
Michael Morrissey <mmorrissey@market76.com>
2014-09-08 22:25:23 -0400

    add express

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 var express = require('express');
 var app = express();
+
+
+//set view directory
+app.set('views', './views')
+app.set('view engine', 'jade')
+
+//routes
+app.get('/', function (req, res) {
+  res.render('index');
+})
+
+//start server
+app.listen(3000)
